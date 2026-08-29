1f18eae213b7a9687fa0ec910efde89d4152c32f
Niranjan Kumar <niranjankumar112005@hotmail.com>
2026-08-30 05:08:50 +0530

    chore: initialize project

---
+++ backend/eslint.config.js
@@ -0,0 +1,14 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  {
+    ignores: ['dist', 'node_modules', 'coverage'],
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [js.configs.recommended, tseslint.configs.recommended, prettier],
+  },
+]);
