75fdc630909cb463c7c6e1d4089b25817e80d238
Niranjan Kumar <niranjankumar112005@hotmail.com>
2026-09-03 01:31:37 +0530

    chore: prepare backend for production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -95,7 +95,7 @@ app.use(
         saveUninitialized: false,
         cookie: {
             httpOnly: true,
-            sameSite: "lax",
+            sameSite: isProduction ? "none" : "lax",
             secure: isProduction,
             maxAge: 1000 * 60 * 60 * 12,
         },
